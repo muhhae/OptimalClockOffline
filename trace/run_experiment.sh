3f823916a50802e6e6c98e613a21e32f8694f79c
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-26 18:37:44 +0700

    added script to run the exp

---
+++ trace/run_experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bash ./download_trace.sh
+
+for file in "./*; do
+    filename="$(basename "$file")"
+    graph_file="../result/graph/${filename%%.*}_128MiB.png"
+
+    if [ -f "$graph_file" ]; then
+        echo "Skipping processing: $filename (corresponding graph exists: $graph_file)"
+    else
+        echo "Processing: $filename"
+        ../build/cacheSimulator . $filename
+    fi
+done
+
+echo "All experiments done!"
