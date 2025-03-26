8251deb6492f2d352d8d4b358f58342a11407447
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-26 18:32:51 +0700

    update download script

--- trace/download_trace.sh
+++ trace/download_trace.sh
@@ -5,12 +5,22 @@ urls=(
     "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/metaCDN/meta_rnha.oracleGeneral.zst"
     "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/alibabaBlock/v2/7.oracleGeneral.zst"
     "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/wiki/wiki_2019t.oracleGeneral.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/cloudphysics/w01.oracleGeneral.bin.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/fiu/fiu_cheetah.cs.fiu.edu-110108-113008.oracleGeneral.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/metaKV/meta_kvcache_traces_1.oracleGeneral.bin.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/metaStorage/block_traces_5.oracleGeneral.bin.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/msr/msr_prxy_1.oracleGeneral.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/systor/2016_LUN1.oracleGeneral.zst"
+    "https://ftp.pdl.cmu.edu/pub/datasets/twemcacheWorkload/cacheDatasets/tencentBlock/v2/traces/1351.oracleGeneral.zst"
 )
 
 for url in "${urls[@]}"; do
     filename="$(basename "$url")"
+    graph_file="../result/graph/${filename%%.*}_128MiB.png"
     if [ -f "$filename" ]; then
         echo "Skipping: $filename (already exists)"
+    elif [ -f "$graph_file" ]; then
+        echo "Skipping download: $filename (corresponding graph exists: $graph_file)"
     else
         echo "Downloading: $url"
         wget "$url" -O "$filename"
