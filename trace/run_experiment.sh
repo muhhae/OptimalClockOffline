e40bd85b5133aa3b725458e3b2b5fd3603b06e4d
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-27 12:18:40 +0700

    feat: batch run script

--- trace/run_experiment.sh
+++ trace/run_experiment.sh
@@ -3,6 +3,9 @@
 bash ./download_trace.sh
 shopt -s nullglob
 
+BATCH_SIZE=4
+batch=()
+
 for file in ./*.oracleGeneral*; do
     filename="$(basename "$file")"
     graph_file="../result/graph/${filename%%.oracleGeneral*}_128MiB.png"
@@ -11,11 +14,22 @@ for file in ./*.oracleGeneral*; do
     if [ -f "$graph_file" ]; then
         echo "Skipping processing: $filename (corresponding graph exists: $graph_file)"
     else
-        echo "Processing: $filename"
-        ../build/cacheSimulator ../result $filename
+        batch+=(filename)
+    fi
+
+    if ((${#batch[@]} == $batch)); then
+        echo "Processing: ${batch[@]}"
+        ../build/cacheSimulator ../result ${batch[@]}
         ../python/.venv/bin/python ../python/csv_to_plot.py
-        git add ../result/**/${filename%%.*}* && git commit -m "Added ${filename%%.*} result (automated)" && git push
+        git add ../result/**/* && git commit -m "Added ${batch[@]} result (automated)" && git push
     fi
 done
+
+if ((${#batch[@]} > 0)); then
+    echo "Processing: ${batch[@]}"
+    ../build/cacheSimulator ../result ${batch[@]}
+    ../python/.venv/bin/python ../python/csv_to_plot.py
+    git add ../result/**/* && git commit -m "Added ${batch[@]} result (automated)" && git push
+fi
 
 echo "All experiments done!"
