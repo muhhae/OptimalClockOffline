36e49c380d9208e7f87c16890220a43166ce568b
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-04 15:59:28 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo apt install gh libglib2.0-dev cmake python3.10-venv &&
-    cd ~ && git clone https://github.com/1a1a11a/libCacheSim &&
+    cd /disk && git clone https://github.com/1a1a11a/libCacheSim &&
     cd libCacheSim && mkdir _build && cd _build && cmake .. &&
     make -j && sudo make install
 
 cd ~ &&
-    cd ~/OptimalClockOffline/python &&
+    cd /disk/OptimalClockOffline/python &&
     python -m venv .venv && .venv/bin/pip install -r requirement.txt &&
-    cd ~/OptimalClockOffline && mkdir build && cd build && cmake .. && make . && cd ../trace &&
+    cd /disk/OptimalClockOffline && mkdir build && cd build && cmake .. && make . && cd ../trace &&
     gh auth login && gh auth setup-git
