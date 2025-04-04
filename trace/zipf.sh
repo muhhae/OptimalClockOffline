3b843c1d1443a8bb6667658dd746ebfd7af4d6ce
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-04 16:56:46 +0700

    better message

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -4,12 +4,13 @@ PYSCRIPT=../../libCacheSim/scripts/data_gen.py
 
 echo "0 0.4 0.8 1 1.4 1.8 2" | xargs -n 1 -P "$(nproc --ignore=2)" bash -c '
     alpha="$1"
+    echo "working on alpha $alpha generated!"
     ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
-    echo "alpha $alpha generated!" &&
+    echo "alpha $alpha generated!"
     ../build/cacheSimulator ../result "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
-    echo "alpha $alpha processed!" &&
+    echo "alpha $alpha processed!"
     ../python/.venv/bin/python ../python/csv_to_plot.py &&
-    echo "alpha $alpha plotted!" &&
+    echo "alpha $alpha plotted!"
     git add ../result/**/* &&
     git commit -m "Added zipf_${alpha}_10_100.oracleGeneral result (automated)" &&
     git push &&
