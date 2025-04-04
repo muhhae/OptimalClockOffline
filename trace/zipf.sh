d4d94b2b57a1803c353112b66b8536f22f8dd262
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-04 16:55:20 +0700

    fix

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -2,15 +2,16 @@
 
 PYSCRIPT=../../libCacheSim/scripts/data_gen.py
 
-echo "0 0.5 1 1.5 2" | xargs -P "$(nproc --ignore=2)" -I {} bash -c '
-    ../python/.venv/bin/python "$1" -m 1000000 -n 100000000 --alpha {} --bin-output "../trace/zipf_{}_10_100.oracleGeneral" &&
-    echo "alpha {} generated!" &&
-    ../build/cacheSimulator ../result "../trace/zipf_{}_10_100.oracleGeneral" &&
-    echo "alpha {} processed!" &&
+echo "0 0.4 0.8 1 1.4 1.8 2" | xargs -n 1 -P "$(nproc --ignore=2)" bash -c '
+    alpha="$1"
+    ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
+    echo "alpha $alpha generated!" &&
+    ../build/cacheSimulator ../result "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
+    echo "alpha $alpha processed!" &&
     ../python/.venv/bin/python ../python/csv_to_plot.py &&
-    echo "alpha {} plotted!" &&
+    echo "alpha $alpha plotted!" &&
     git add ../result/**/* &&
-    git commit -m "Added zipf_{}_10_100.oracleGeneral result (automated)" &&
+    git commit -m "Added zipf_${alpha}_10_100.oracleGeneral result (automated)" &&
     git push &&
-    echo "alpha {} pushed!"
-' _ "$PYSCRIPT"
+    echo "alpha $alpha pushed!"
+' _
