0a774a8a9ff97b4a2985eefd30fefd2698e34da3
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-04 16:53:36 +0700

    fix

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -2,7 +2,7 @@
 
 PYSCRIPT=../../libCacheSim/scripts/data_gen.py
 
-echo "0 0.4 0.8 1 1.4 1.8 2" | xargs -n 1 -P "$(nproc --ignore=2)" -I {} bash -c '
+echo "0 0.5 1 1.5 2" | xargs -P "$(nproc --ignore=2)" -I {} bash -c '
     ../python/.venv/bin/python "$1" -m 1000000 -n 100000000 --alpha {} --bin-output "../trace/zipf_{}_10_100.oracleGeneral" &&
     echo "alpha {} generated!" &&
     ../build/cacheSimulator ../result "../trace/zipf_{}_10_100.oracleGeneral" &&
