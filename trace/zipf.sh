dc81c9e0b09a3e8e222d1b4e0cf058ae3f3c957b
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-04 17:36:46 +0700

    fix path

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -5,9 +5,9 @@ PYSCRIPT=../../libCacheSim/scripts/data_gen.py
 echo "0 0.3 0.7 1 1.3 1.7 2" | xargs -n 1 -P "$(nproc --ignore=2)" bash -c '
     alpha="$1"
     echo "working on alpha $alpha!" &&
-    ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
+    ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "zipf_${alpha}_10_100.oracleGeneral" &&
     echo "alpha $alpha generated!" &&
-    ../build/cacheSimulator ../result "../trace/zipf_${alpha}_10_100.oracleGeneral" &&
+    ../build/cacheSimulator ../result "zipf_${alpha}_10_100.oracleGeneral" &&
     echo "alpha $alpha processed!" &&
     ../python/.venv/bin/python ../python/csv_to_plot.py &&
     echo "alpha $alpha plotted!" &&
