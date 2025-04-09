5b7888faf7914867d37d30edeb6f2409cfad6bef
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-09 22:04:15 +0700

    update script

--- trace/run_experiment.sh
+++ trace/run_experiment.sh
@@ -3,7 +3,8 @@
 shopt -s nullglob
 
 offline_clock() {
-    cache_sizes=(0.001 0.01 0.1 0.2 0.4)
+    fixed_cache_sizes=(128 256 512 1024 2048)
+    relative_cache_sizes=(0.001 0.01 0.1 0.2 0.4)
     max_iteration=20
 
     file=$1
@@ -13,7 +14,7 @@ offline_clock() {
     filename="$(basename "$file")"
     basename="${filename%%.oracleGeneral*}"
 
-    for cache_size in "${cache_sizes[@]}"; do
+    for cache_size in "${relative_cache_sizes[@]}"; do
         log_file="../result/log/${basename}[${cache_size}].csv"
         if ! [ -s $log_file ]; then
             do_cache_sizes+=($cache_size)
@@ -21,9 +22,18 @@ offline_clock() {
             echo "Skipping processing: $filename $cache_size (corresponding result exists and not empty: $log_file)"
         fi
     done
-    echo ${do_cache_sizes[@]}
+
+    for cache_size in "${fixed_cache_sizes[@]}"; do
+        log_file="../result/log/${basename}[${cache_size}MiB].csv"
+        if ! [ -s $log_file ]; then
+            do_cache_sizes+=($cache_size)
+        else
+            echo "Skipping processing: $filename $cache_size (corresponding result exists and not empty: $log_file)"
+        fi
+    done
 
     echo "Processing: $filename"
+    echo "Cache Size: ${do_cache_sizes[@]}"
 
     ../build/cacheSimulator $filename -o ../result -r ${do_cache_sizes[@]} -i $max_iteration &&
     ../python/.venv/bin/python ../python/csv_to_plot.py &&
