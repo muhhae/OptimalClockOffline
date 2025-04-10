0f21813f41f550a879a85ac40d4afebf3f85b445
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-10 11:16:55 +0700

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@
 sudo apt install gh libglib2.0-dev cmake python3.10-venv &&
 cd .. && git clone https://github.com/1a1a11a/libCacheSim &&
 cd libCacheSim && mkdir _build && cd _build && cmake .. &&
-make -j && sudo make install
-
-cd ../..
-
+make -j && sudo make install &&
+cd ../.. &&
 cd OptimalClockOffline/python &&
 python -m venv .venv && .venv/bin/pip install -r requirement.txt &&
 cd .. && mkdir build && cd build && cmake .. && make . && cd ../trace &&
