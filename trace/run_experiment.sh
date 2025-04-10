fbf8d749a7dd31a586f0acad7b354a4f140b0d7f
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-10 12:28:45 +0700

    fix git command

--- trace/run_experiment.sh
+++ trace/run_experiment.sh
@@ -48,7 +48,7 @@ offline_clock() {
 
     ../python/.venv/bin/python ../python/csv_to_plot.py
     ../python/.venv/bin/python ../python/result_to_md.py
-    git pull --rebase && git add ../result/* && git commit -m "Update result (automated)" && git push
+    git add ../result/* && git commit -m "Update result (automated)" && git pull --rebase && git push
 }
 
 export -f offline_clock
