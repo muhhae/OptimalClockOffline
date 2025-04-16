de418de621df970ffccc5e4219aa7737c102e92b
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-16 07:28:15 +0700

    download all of traces

---
+++ download_task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+input_file="traces.txt"
+output_file="~/task"
+
+while IFS= read -r link; do
+    if [ -z "$link" ] || [[ "$link" == \#* ]]; then
+        continue
+    fi
+    filename=$(basename $link)
+    echo "shell:1:1:1:wget -O /mnt/gv1/traces/$filename $link" >> $output_file
+
+done < "$input_file"
+
+echo "Finished adding task to $output_file."
