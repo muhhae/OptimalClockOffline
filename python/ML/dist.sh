7d2a9a4b9be13cf920f1b17a1a71df16536a2fd3
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-23 12:35:20 +0700

    update script

--- python/ML/dist.sh
+++ python/ML/dist.sh
@@ -16,6 +16,7 @@ for size in ${relative_size[@]}; do
     fi
     datasets=${datasets%?}
     ram_usage=$(python -c "import common;common.AddDatasets($datasets);import var;print(int(var.df.memory_usage(deep=True).sum()/1024**3))")
+    ram_usage=$((ram_usage+2))
     echo "Ram Usage: $ram_usage"
-    echo "shell:1:$ram_usage:2:python -c \"import common as c;c.AddDatasets($datasets);c.SetupData();c.SetupModel(1000);c.Train();c.SaveModel($out_dir/model\[$size\].pkl)\""
+    echo "shell:1:$ram_usage:2:python -c \"import common as c;c.AddDatasets($datasets);c.SetupData();c.SetupModel(1000);c.Train();c.SaveModel($out_dir/model\[$size\].pkl)\"" >> ~/task
 done
