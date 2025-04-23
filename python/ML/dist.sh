149094f6968345948af4271fb4ed64362f3a6cdc
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-23 11:59:58 +0700

    script

---
+++ python/ML/dist.sh
@@ -0,0 +1,20 @@
+datasets_dir=$1
+out_dir=$2
+if [ -z "$datasets_dir" ] || [ -z "$out_dir" ]; then
+    echo "Usage: bash dist.sh [datasets_dir] [out_dir]"
+    exit 1
+fi
+relative_size=(0.001 0.01 0.1 0.2 0.4)
+shopt -s nullglob
+for size in ${relative_size[@]}; do
+    datasets=""
+    for file in $datasets_dir/*\["$size"\].csv; do
+        datasets+="\"$file\","
+    done
+    if [ -z "$datasets" ]; then
+        continue
+    fi
+    datasets=${datasets%?}
+    ram_usage=$(python -c "import common;common.AddDatasets($datasets);print(int(common.df.memory_usage(deep=True).sum()/1024**3))")
+    echo "Ram Usage: $ram_usage"
+done
