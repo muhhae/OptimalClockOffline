925b579765632c28ab590a062669fc77e44ac43c
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-27 10:51:49 +0700

    typehint

--- python/ML/var.py
+++ python/ML/var.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from sklearn.base import BaseEstimator
 
 df: pd.DataFrame = None
 
-model = None
+model: BaseEstimator = None
 
 X_train: pd.DataFrame = None
 y_train: pd.DataFrame = None
