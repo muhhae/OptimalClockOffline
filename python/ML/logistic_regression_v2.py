f831f606e1f146a0d35f70dd443c2bcdc448a50d
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-04-27 10:51:33 +0700

    change clock_time to time_since

--- python/ML/logistic_regression_v2.py
+++ python/ML/logistic_regression_v2.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 import var
 import common as c
@@ -11,11 +12,18 @@ def SetupModel(max_iter: int = 1000):
 
 
 def SetupData():
-    if var.df is None:
-        raise Exception("Datasets has not been loaded, exec AddDatasets")
-    var.df["obj_size_percent"] = var.df["obj_size"] / var.df["cache_size"] * 1e6
-    var.df.drop(["obj_size", "cache_size"], axis=1, inplace=True)
     c.SetupData()
+    time_begin = (
+        var.X_train["clock_time"].min()
+        if var.X_train["clock_time"].min() < var.X_test["clock_time"].min()
+        else var.X_test["clock_time"].min()
+    )
+    for x in [var.X_train, var.X_test]:
+        x["time_since"] = x["clock_time"] - time_begin
+        x["obj_size_relative"] = (x["obj_size"] / x["cache_size"] * 1e6).astype(
+            np.int64
+        )
+        x.drop(["obj_size", "cache_size", "clock_time"], axis=1, inplace=True)
 
 
 Train = c.Train
