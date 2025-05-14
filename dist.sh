900fa9d0bc1f637ec289b35826334b737b842dc7
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-14 22:43:17 +0700

    update script

--- dist.sh
+++ dist.sh
@@ -28,9 +28,17 @@ while IFS= read -r link; do
     min_dram=$(( gb+1 ))
 
     for cache_size in "${relative_cache_sizes[@]}"; do
-        datasets="$out_dir/datasets/${basename}[${cache_size},ignore_obj_size,TEST].csv"
+        result_file="$out_dir/log/${basename}[${cache_size},ignore_obj_size,TEST].csv"
+        if ! [ -s $result_file ]; then
             echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration --ignore-obj-size -d ignore_obj_size,TEST" >> ~/task
-        datasets="$out_dir/datasets/${basename}[${cache_size},TEST].csv"
+        else
+            echo "Skipping processing: $filename $cache_size (corresponding result exists and not empty: $result_file)"
+        fi
+        result_file="$out_dir/datasets/${basename}[${cache_size},TEST].csv"
+        if ! [ -s $result_file ]; then
             echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration -d TEST" >> ~/task
+        else
+            echo "Skipping processing: $filename $cache_size (corresponding result exists and not empty: $result_file)"
+        fi
     done
 done < "$traces_txt"
