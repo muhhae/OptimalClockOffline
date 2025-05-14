321c30fc876e39e33fda5dc56396cd4a55e94691
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-14 13:35:54 +0700

    update zipf script

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -4,6 +4,6 @@ PYSCRIPT=../../libCacheSim/scripts/data_gen.py
 
 echo "0 0.3 0.7 1 1.3 1.7 2" | xargs -n 1 -P "$(nproc --ignore=2)" bash -c '
     alpha="$1"
-    ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "zipf_${alpha}_10_100.oracleGeneral" &&
-    echo "alpha $alpha generated!"
+    ../python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output "zipf_${alpha}.oracleGeneral" &&
+    echo "zipf_${alpha}.oracleGeneral generated!"
 ' _
