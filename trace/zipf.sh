c6c6535c671113088d453467b33ff44acec3c1ad
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-14 13:49:57 +0700

    fix

--- trace/zipf.sh
+++ trace/zipf.sh
@@ -5,6 +5,6 @@ DIR_OUTPUT=/mnt/v0/traces
 
 echo "0 0.3 0.7 1 1.3 1.7 2" | xargs -n 1 -P "$(nproc --ignore=2)" bash -c '
     alpha="$1"
-    ~/OptimalClockOffline/python/.venv/bin/python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output ""'"$DIR_OUTPUT/"'"zipf_${alpha}.oracleGeneral" &&
+    python "'"$PYSCRIPT"'" -m 1000000 -n 100000000 --alpha "$alpha" --bin-output ""'"$DIR_OUTPUT/"'"zipf_${alpha}.oracleGeneral" &&
     echo ""'"$DIR_OUTPUT/"'"zipf_${alpha}.oracleGeneral generated!"
 ' _
