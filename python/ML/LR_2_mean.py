e475fa8ce88b688a24065b80f2ba1bde287541f0
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-15 19:36:56 +0700

    add LR_2 models

---
+++ python/ML/LR_2_mean.py
@@ -0,0 +1,35 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+import var
+import common as c
+
+
+def SetupModel(max_iter: int = 1000):
+    var.model = LogisticRegression(
+        max_iter=max_iter, class_weight="balanced", n_jobs=-1
+    )
+    var.cols = [
+        "vtime_since_log",
+        "clock_freq_norm",
+        "wasted",
+    ]
+    var.input_dtype = np.float32
+
+
+def SetupData():
+    c.SetupData()
+
+
+Train = c.Train
+
+# Hardly Need Modification
+Test = c.Test
+AddDatasets = c.AddDatasets
+DescribeData = c.DescribeData
+ExportONNX = c.ExportONNX
+LoadONNX = c.LoadONNX
+SaveModel = c.SaveModel
+LoadModel = c.LoadModel
+PlotSave = c.PlotSave
+PlotShow = c.PlotShow
