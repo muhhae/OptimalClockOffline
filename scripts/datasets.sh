5ca8a7c92f8a453e709bb70c986f8bf6146aa4c3
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-20 21:36:05 +0700

    update scripts for generating datasets

--- scripts/datasets.sh
+++ scripts/datasets.sh
@@ -1,36 +1,48 @@
 relative_cache_sizes=(0.001 0.01 0.1 0.2 0.4)
 max_iteration=2
-basedir="/mnt/v0"
 
-traces_txt="$1"
-if [[ -z $traces_txt ]]; then
-  echo "[arg1] traces_txt required"
+traces_dir=""
+traces_in=""
+result_out=""
+task_out=""
+add_desc=""
+
+usage() {
+  echo "Usage: $0 [-d traces_dir] [-i traces_in] [-r result_out] [-t task_out] [-a add_desc]"
   exit 1
+}
+
+while getopts "d:i:r:t:a:" opt; do
+  case $opt in
+    d) traces_dir="$OPTARG" ;;
+    i) traces_in="$OPTARG" ;;
+    r) result_out="$OPTARG" ;;
+    t) task_out="$OPTARG" ;;
+    a) add_desc="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+if [ -z "$traces_dir" ] || [ -z "$traces_in" ] || [ -z "$result_out" ] || [ -z "$task_out" ]; then
+    usage
 fi
 
-out_dir="$2"
-if [[ -z $out_dir ]]; then
-  out_dir="./result"
-fi
-
-rm ~/task
+echo "" > $task_out
 
 while IFS= read -r link; do
     if [ -z "$link" ] || [[ "$link" == \#* ]]; then
         continue
     fi
+
     filename=$(basename $link)
-    file="$basedir/traces/$filename"
+    file="$traces_dir/$filename"
     basename="${filename%%.oracleGeneral*}"
     size=$(stat --format="%s" "$file")
-
     gb=$(( (size + 1024*1024*1024 - 1) / (1024*1024*1024) ))
     min_dram=$(( gb+1 ))
 
     for cache_size in "${relative_cache_sizes[@]}"; do
-        datasets="$out_dir/datasets/${basename}[${cache_size},ignore_obj_size].csv"
-        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration --generate-datasets --ignore-obj-size -d ignore_obj_size && python ~/OptimalClockOffline/python/describe_csv.py $datasets > $datasets.desc" >> ~/task
-        datasets="$out_dir/datasets/${basename}[${cache_size}].csv"
-        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration --generate-datasets && python ~/OptimalClockOffline/python/describe_csv.py $datasets > $datasets.desc" >> ~/task
+        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration --generate-datasets --ignore-obj-size -d ignore_obj_size $add_desc" >> $task_out
+        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -o $out_dir -r $cache_size -i $max_iteration --generate-datasets -d $add_desc" >> $task_out
     done
 done < "$traces_txt"
