000a0bdc5443c0953b33675844e9655f93c64887
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-05-22 16:44:05 +0700

    Added model LR_4

---
+++ python/ML/LR_4_mean.py
@@ -0,0 +1,42 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+import var
+import common as c
+
+
+def SetupModel(max_iter: int = 1000):
+    var.model = LogisticRegression(
+        max_iter=max_iter, class_weight="balanced", n_jobs=-1
+    )
+    var.cols = [
+        "rtime_since_log",
+        "vtime_since_log",
+        "rtime_between_log",
+        "clock_freq_std",
+        "lifetime_freq_std",
+    ]
+    var.input_dtype = np.float32
+
+
+def SetupData():
+    c.SetupData()
+
+
+Train = c.Train
+
+# Hardly Need Modification
+AddDatasets = c.AddDatasets
+ResetDatasets = c.ResetDatasets
+LoadDatasets = c.LoadDatasets
+SetTestData = c.SetTestData
+SetTrainData = c.SetTrainData
+SplitData = c.SplitData
+Test = c.Test
+DescribeData = c.DescribeData
+ExportONNX = c.ExportONNX
+LoadONNX = c.LoadONNX
+SaveModel = c.SaveModel
+LoadModel = c.LoadModel
+PlotSave = c.PlotSave
+PlotShow = c.PlotShow
