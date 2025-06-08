b51662a08ae6c4040bafe219a87499de04105807
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-08 11:39:02 +0700

    Manually tuned the weight of the class

---
+++ python/ML/LR_decay_rtime_manual_weight_tuned.py
@@ -0,0 +1,30 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+import var
+import common as c
+from common import *
+
+
+def SetupModel(max_iter: int = 1000):
+    steps = [
+        ("scaler", RobustScaler()),
+        (
+            "model",
+            LogisticRegression(
+                max_iter=max_iter,
+                class_weight={
+                    0: 1,
+                    1: 0.5,
+                },
+                random_state=42,
+            ),
+        ),
+    ]
+    var.model = Pipeline(steps)
+    var.inputs = [
+        "clock_freq_decayed_rtime",
+    ]
+    var.input_dtype = np.float32
