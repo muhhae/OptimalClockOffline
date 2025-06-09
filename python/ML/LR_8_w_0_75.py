59427cb8c6a149b2182d826df5491971e7c2eccb
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-09 10:33:53 +0700

    Renaming models and adding new models

---
+++ python/ML/LR_8_w_0_75.py
@@ -0,0 +1,32 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+import var
+import common as c
+from common import *
+
+
+def SetupModel(max_iter: int = 1000):
+    steps = [
+        ("scaler", RobustScaler()),
+        (
+            "model",
+            LogisticRegression(
+                max_iter=max_iter,
+                class_weight={
+                    0: 1,
+                    1: 0.75,
+                },
+                random_state=42,
+            ),
+        ),
+    ]
+    var.model = Pipeline(steps)
+    var.inputs = [
+        "rtime_since_log",
+        "clock_freq",
+        "lifetime_freq",
+    ]
+    var.input_dtype = np.float32
