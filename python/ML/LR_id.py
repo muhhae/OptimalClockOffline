0cf4b04d606599ce1b151596b61b20795e75749e
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-11 10:31:23 +0700

    Added models that only use obj id

---
+++ python/ML/LR_id.py
@@ -0,0 +1,27 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import RobustScaler
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+import var
+import common as c
+from common import *
+
+
+def SetupModel(max_iter: int = 1000):
+    steps = [
+        ("scaler", RobustScaler()),
+        (
+            "model",
+            LogisticRegression(
+                max_iter=max_iter,
+                class_weight="balanced",
+                random_state=42,
+            ),
+        ),
+    ]
+    var.model = Pipeline(steps)
+    var.inputs = [
+        "obj_id",
+    ]
+    var.input_dtype = np.float32
