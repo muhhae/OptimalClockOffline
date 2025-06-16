9867220950ac05ba135b7b05896da436127f87a7
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-06-16 14:05:52 +0700

    fix additional parameter and change it to p

--- scripts/dist-cachesim.sh
+++ scripts/dist-cachesim.sh
@@ -3,11 +3,11 @@
 relative_cache_sizes=(0.005 0.01 0.1 0.25 0.5)
 
 usage() {
-    echo "Usage: bash $0 -r t[r]aces_txt -d traces_[d]ir -o [o]ut_dir -a [a]dd_desc -t [t]ask_out -g al[g]orithm -m add_para[m] -i "
+    echo "Usage: bash $0 -r t[r]aces_txt -d traces_[d]ir -o [o]ut_dir -a [a]dd_desc -t [t]ask_out -g al[g]orithm -p add_[p]aram"
     exit 1
 }
 
-while getopts "d:o:a:t:r:g:m:" opt; do
+while getopts "d:o:a:t:r:g:p:" opt; do
     case $opt in
         d) traces_dir="$OPTARG" ;;
         o) out_dir="$OPTARG" ;;
@@ -15,7 +15,7 @@ while getopts "d:o:a:t:r:g:m:" opt; do
         r) traces_txt="$OPTARG" ;;
         a) add_desc="$OPTARG" ;;
         g) algorithm="$OPTARG" ;;
-        m) add_param="$OPTARG" ;;
+        p) add_param="$OPTARG" ;;
         *) usage ;;
     esac
 done
@@ -41,7 +41,7 @@ while IFS= read -r link; do
     min_dram=$(( gb+1 ))
 
     for cache_size in "${relative_cache_sizes[@]}"; do
-        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -a $algorithm $m -o $out_dir -r $cache_size --ignore-obj-size -d ignore_obj_size,$algorithm$add_desc" >> $task_out
-        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -a $algorithm $m -o $out_dir -r $cache_size -d $algorithm$add_desc" >> $task_out
+        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -a $algorithm $add_param -o $out_dir -r $cache_size --ignore-obj-size -d ignore_obj_size,$algorithm$add_desc" >> $task_out
+        echo "shell:1:$min_dram:1:~/OptimalClockOffline/build/cacheSimulator $file -a $algorithm $add_param -o $out_dir -r $cache_size -d $algorithm$add_desc" >> $task_out
     done
 done < "$traces_txt"
